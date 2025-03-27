5454bd3a1e83a57b80f4bdf46249bb16c6d3f7ae
Giorgio Maria Federico Birnthaler <129273127+Dodothereal@users.noreply.github.com>
2025-03-27 11:06:59 +0100

    Hallo Philip

--- public/nmain.js
+++ public/nmain.js
@@ -1,7 +1,7 @@
 import * as THREE from 'https://unpkg.com/three@0.137.0/build/three.module.js';
 import { OrbitControls } from 'https://unpkg.com/three@0.137.0/examples/jsm/controls/OrbitControls.js';
 
-
+Hallo Philip 
 export class HallEffectSimulation {
     constructor() {
         this.scene = new THREE.Scene();
